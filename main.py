f99122570556033f5c707d8e9f8bc917848aa4e8
Denysmak <denysmakene13@gmail.com>
2023-08-24 12:45:00 -0300

    adiciona background

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ pygame.init()
 #create the screen
 screen = pygame.display.set_mode((800, 600))
 
+#Background
+background = pygame.image.load('./Assets/Space.png')
+background_image = pygame.transform.scale(background, (840, 600))
+
+
 #Title and Icon
 pygame.display.set_caption("Space invaders")
 icon = pygame.image.load('./Assets/nave-espacial.png')
@@ -24,7 +29,7 @@ nova_imagem = pygame.transform.scale(playerImg, (75, 75))
 enemyImg = pygame.image.load('./Assets/alien.png')
 enemyX = random.randint(0, 777)
 enemyY = random.randint(50, 150)
-enemyX_change = 0.3
+enemyX_change = 3.5
 enemyY_change = 40
 
 enemy_imagem = pygame.transform.scale(enemyImg, (45, 45))
@@ -43,6 +48,8 @@ running = True
 while running:
     # RGB - Red, Green, Blue
     screen.fill((0,0,0))
+    #Background Image
+    screen.blit(background_image, (0,0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,9 +58,9 @@ while running:
     #if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                playerX_change = -0.3
+                playerX_change = -5
             if event.key == pygame.K_d:
-                playerX_change = 0.3
+                playerX_change = 5
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_d:
                 print("Keystroke has been released")
@@ -70,10 +77,10 @@ while running:
     #enemy movement
     enemyX += enemyX_change
     if enemyX <= 0:
-        enemyX_change = 0.3
+        enemyX_change = 3.5
         enemyY += enemyY_change
     elif enemyX >= 755:
-        enemyX_change = -0.3
+        enemyX_change = -3.5
         enemyY += enemyY_change
 
     player(playerX, playerY)
